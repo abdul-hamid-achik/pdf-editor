ed6151f71c0eaf38f2ab26555d33fa73cfe32ed0
abdul hamid <abdulachik@icloud.com>
2025-08-29 21:52:38 -0600

    Refactor: integrate @hotwired/stimulus-webpack-helpers for automatic controller loading in index.js and add dependency in package.json

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,16 +1,5 @@
-// Import and register all your controllers manually
 import { application } from "./application"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
-// Import all controllers
-import HelloController from "./hello_controller"
-import DocumentListController from "./document_list_controller"
-import PdfEditorController from "./pdf_editor_controller"
-import PdfPreviewController from "./pdf_preview_controller"
-import ThemeToggleController from "./theme_toggle_controller"
-
-// Register controllers
-application.register("hello", HelloController)
-application.register("document-list", DocumentListController)
-application.register("pdf-editor", PdfEditorController)
-application.register("pdf-preview", PdfPreviewController)
-application.register("theme-toggle", ThemeToggleController)
+const context = require.context(".", true, /\.js$/)
+application.load(definitionsFromContext(context))
